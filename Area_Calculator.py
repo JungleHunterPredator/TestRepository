3130cc2246dab99189c5f5682f4cd462562d59d5
JungleHunterPredator <kingsley.yam.zhen@gmail.com>
2023-09-09 14:09:03 +0800

    Updated Code

--- Area_Calculator.py
+++ Area_Calculator.py
@@ -1,7 +1,5 @@
 import math
 
-print("Welcome to an Area and Volume Calulator!")
-
 # Shapes (2D): Square, Rectangle, Triangle, Circle, Pentagon, Hexagon, Octagon
 # Shapes (3D): Cube, Rectangular Prism, Cylinder, Shpere, Pyramid, Cone
 
@@ -63,3 +61,85 @@ def calCylinder():
     h = int(input("enter the height of the Cylinder (cm): "))
     volume = math.pi * (r * r) * h
     return volume
+
+def calSphere():
+    r = int(input("enter the radius of the Sphere (cm): "))
+    volume = 4.0 / 3.0 * math.pi * r**3
+    return volume
+
+def calPyramid():
+    l = int(input("enter the base length of the Pyramid (cm): "))
+    w = int(input("enter the base width of the Pyramid (cm): "))
+    h = int(input("enter the height of the Pyramid (cm): "))
+    volume = (l * w * h) / 3
+    return volume
+
+def calCone():
+    r = int(input("enter the radius of the Cone (cm): "))
+    h = int(input("enter the height of the Cone (cm): "))
+    volume = (math.pi * r * r * h) / 3
+    return volume
+
+def menu2D():
+    while True:
+        print("-- Calculate Area --")
+        print("a. Square")
+        print("b. Rectangle")
+        print("c. Triangle")
+        print("d. Circle")
+        print("e. Pentagon")
+        print("f. Hexagon")
+        print("g. Octogon")
+        print("m. main menu")
+        print("q. quit\n")
+
+        usrIn = input("Enter command: ")
+        match usrIn:
+            case "a":
+                print("")
+                area = calSquare()
+                print("The area of the Square:", area, "\n")
+            case "b":
+                print("")
+                area = calRectangle()
+            case "c":
+                print("")
+                area = calTriangle()
+            case "d":
+                print("")
+                area = calCircle()
+            case "e":
+                print("")
+                area = calPentagon()
+            case "f":
+                print("")
+                area = calHexagon()
+            case "g":
+                print("")
+                area = calOctagon()
+            case "m":
+                main()
+            case "q":
+                exit()
+
+def menu3D():
+    print("-- Calculate Volume --")
+    print("a. Cube")
+    print("b. Rectangle Prism")
+    print("c. Cylinder")
+    print("d. Sphere")
+    print("e. Pyramid")
+    print("f. Cone")
+    print("m. main menu")
+    print("q. quit\n")
+
+def main():
+    print("Welcome to an Area and Volume Calulator!\n")
+    usrSelect = input("What is the type of shape you want (2D / 3D): ").upper
+    match usrSelect:
+        case "2D":
+            menu2D()
+        case "3D":
+            menu3D()
+
+menu2D()

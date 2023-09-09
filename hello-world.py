6d63f78dbff0be04047fa39670d2753ac3a9c5f8
JungleHunterPredator <kingsley.yam.zhen@gmail.com>
2023-09-09 11:52:00 +0800

    Python File

---
+++ hello-world.py
@@ -0,0 +1,18 @@
+print("Hello World")
+
+height = int(input("Enter the height of the shape (cm): "))
+length = int(input("Enter the length of the shape (cm): "))
+depth = int(input("Enter the depth of the shape (cm): "))
+
+square = length * height
+cube = square * depth
+
+print("")
+print("--------------------------------->")
+print("Info on the shape: ")
+print("height:", height, "cm")
+print("length:", length, "cm")
+print("depth:", depth, "cm")
+print("area:", square, "cm2")
+print("volume:", cube, "cm3")
+print("")
